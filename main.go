8a96c1ee8f5d563d1767f82a31253b305dd81076
JDMukiibs <joshuamukiibi10@gmail.com>
2024-07-17 21:50:03 +0300

    adjusting after comparing with solution

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	const port = "8080"
+	// This creates a new HTTP request multiplexer
 	httpServerMux := http.NewServeMux()
+	httpServerMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
-		Addr:    "localhost:8080",
+		Addr:    "localhost:" + port,
 		Handler: httpServerMux,
 	}
-	server.ListenAndServe()
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
