55d1331566f0eab5bfe9f1ddf82feb805e5f13c9
JDMukiibs <joshuamukiibi10@gmail.com>
2024-07-18 19:39:16 +0300

    complete: initial server basics

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ func main() {
 
 	// This creates a new HTTP request multiplexer
 	httpServerMux := http.NewServeMux()
-	httpServerMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	httpServerMux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	httpServerMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: httpServerMux,
