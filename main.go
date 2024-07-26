0135560b9377520933aa77713573d8d8c6e247ea
JDMukiibs <joshuamukiibi10@gmail.com>
2024-07-26 17:49:50 +0300

    complete: routing w/ stateful handlers

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileServerHits int
+}
+
 func main() {
+	log.SetPrefix("chirpy > ")
 	const filepathRoot = "."
 	const port = "8080"
+	apiCfg := &apiConfig{}
 
 	// This creates a new HTTP request multiplexer
 	httpServerMux := http.NewServeMux()
-	httpServerMux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	httpServerMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	httpServerMux.Handle("/app/*", apiCfg.middlewareMetricsInc(handler))
+	httpServerMux.HandleFunc("/healthz", handlerReadiness)
+	httpServerMux.HandleFunc("/metrics", apiCfg.middlewareMetrics)
+	httpServerMux.HandleFunc("/reset", apiCfg.middlewareMetricsReset)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: httpServerMux,
@@ -24,4 +30,30 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits++
+		w.Header().Add("Cache-Control", "no-cache")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileServerHits)))
+}
+
+func (cfg *apiConfig) middlewareMetricsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits have been reset to %d", cfg.fileServerHits)))
 }
